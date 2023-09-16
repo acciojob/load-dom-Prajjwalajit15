25daf80f49036dcdb29c451180fc212c726baba4
prajjwal ajit <122018880+Prajjwalajit15@users.noreply.github.com>
2023-09-16 13:37:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+ // Add an event listener to the "DOMContentLoaded" event
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to add text to the body when the DOM is loaded
+    function addTextToBody() {
+        // Create a new text node with the text "DOM load success"
+        var textNode = document.createTextNode("DOM load success");
+
+        // Get the body element
+        var body = document.body;
+
+        // Remove any existing children from the body (as per your requirement)
+        while (body.firstChild) {
+            body.removeChild(body.firstChild);
+        }
+
+        // Append the text node to the body
+        body.appendChild(textNode);
+    }
+
+    // Call the function to add text to the body
+    addTextToBody();
+});
